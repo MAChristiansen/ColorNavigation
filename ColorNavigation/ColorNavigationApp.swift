fb3070f7f5fe40f395d626b9c8df97ea78cf309e
Marcus Christiansen <mac080497@gmail.com>
2023-10-29 21:04:03 +0100

    Initial project commit

---
+++ ColorNavigation/ColorNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorNavigationApp.swift
+//  ColorNavigation
+//
+//  Created by Marcus August Christensen on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
